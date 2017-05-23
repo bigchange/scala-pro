5c881e85ac5749aa78916d2b802d955961ee9d34
shanghai-Jerry <cjyou20110920sspu@163.com>
2017-05-23 11:50:32 +0800

    [update] need to fix netty conflict, what a f

--- src/main/scala/com/grpc/GrpcMainClient.scala
+++ src/main/scala/com/grpc/GrpcMainClient.scala
@@ -9,6 +9,8 @@ object GrpcMainClient {
 
   def main(args: Array[String]): Unit = {
     val Array(host, msg) = args
+    // val host = "localhost"
+    // val msg = "java"
     val port = 50051
     val client = GreeterClient.apply(host, port)
     client.greeter(msg)
