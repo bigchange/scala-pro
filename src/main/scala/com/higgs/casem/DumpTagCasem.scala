e972749d1fc0c428cedf8e51682b09c79af03c9e
shanghai-Jerry <cjyou20110920sspu@163.com>
2019-03-13 11:24:58 +0800

    fix

--- src/main/scala/com/higgs/casem/DumpTagCasem.scala
+++ src/main/scala/com/higgs/casem/DumpTagCasem.scala
@@ -66,6 +66,10 @@ object DumpTagCasem {
 
   }
 
+  def recover_data(input:String): Unit = {
+
+  }
+
   def main(args: Array[String]): Unit = {
 
     val (url, user, ps, ftype, ts, out) = (args(0), args(1), args(2), args(3),args(4), args(5))
